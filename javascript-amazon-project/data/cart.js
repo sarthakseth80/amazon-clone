e7c4809f6da7b4d861c918503c0d675559e0201e
SarthakSeth <sarthakseth80@gmail.com>
2026-05-11 21:05:48 +0530

    Generated HTML through javascript using DOM

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,4 +1,10 @@
-export const cart = []; 
+export const cart = [{
+   productId :  "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",       // insert to Cart array if unique product Selected
+      quantity : 2
+},{
+   productId :  "15b6fc6f-327a-4ec4-896f-486349e85a3d",       // insert to Cart array if unique product Selected
+      quantity : 1
+} ]; 
 // All cart functalities here
 
 export function addToCart(productId){  // Exported so that we can use this in amazon.js file 
